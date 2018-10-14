95c783dfd126d74add60b921bfdda2c30d3a95d5
Brian Batchelor <brian.batchelor@insidethesandbox.com>
2018-10-14 16:49:24 -0400

    Feature: node-notes added arguments input

--- nodejs/notes-node/app.js
+++ nodejs/notes-node/app.js
@@ -1,8 +1,24 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 6 */ 
 
-console.log('Starting app');
+console.log("Starting app");
 
-const fs = require('fs');
-const _ = require('lodash');
+const fs = require("fs");
+const _ = require("lodash");
 
-const notes = require('./notes.js');
+const notes = require("./notes.js");
+
+let command = process.argv[2];
+console.log("Command: ", command);
+console.log(process.argv);
+
+if (command === "add"){
+    console.log("Adding new note");
+} else if (command === "list"){
+    console.log("Listing all notes");
+} else if (command === "read"){
+    console.log("Fetching  note");
+} else if (command === "remove"){
+    console.log("Removing note");
+} else {
+    console.log("Command not recognized");
+}
