1ca0f67b7704b19a809434a277496a9ec94bcfbd
Brian Batchelor <brian.batchelor@insidethesandbox.com>
2018-10-14 15:51:50 -0400

    CHORE: clean projects

--- nodejs/notes-node/notes.js
+++ nodejs/notes-node/notes.js
@@ -5,9 +5,3 @@ module.exports.addNote = () => {
     console.log('add note');
     return 'new note';
 };
-
-module.exports.add = (a,b)=>{
-    let res = a+b;
-    console.log(res);
-    return res;
-};
